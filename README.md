b7a556b04521dfd8d8f95f9c38323bdf80c25729
rossreed <ross.areed88@gmail.com>
2014-06-17 15:47:57 -0400

    added functionality for object reporting

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # S32CSV
 S32CSV is a quick and dirty shell script that utilizes the AWS CLI to create a report of all your S3 buckets and their attributes organized in either CSV or TSV format. The default location for the exported data is a file named `report.csv` in your current working directory
+
 ## Requirements
 * OSX or Linux
 * [AWS CLI](http://aws.amazon.com/cli/)
 * [jq](http://stedolan.github.io/jq/)
-* IAM User with **at least** `s3:ListAllMyBuckets`, `s3:GetBucketLocation`, and `s3:GetBucketWebsite` permission on all your buckets. Here is an example IAM policy that will apply the minimum required permissions.
+* IAM User with **at least** `s3:ListAllMyBuckets`, `s3:GetBucketLocation`, `s3:ListBucket` and `s3:GetBucketWebsite` permission on all your buckets. Here is an example IAM policy that will apply the minimum required permissions.
 
 ```json
 {
@@ -14,7 +15,8 @@ S32CSV is a quick and dirty shell script that utilizes the AWS CLI to create a r
             "Action": [
                 "s3:ListAllMyBuckets",
                 "s3:GetBucketLocation",
-                "s3:GetBucketWebsite"
+                "s3:GetBucketWebsite",
+                "s3:ListBucket"
             ],
             "Resource": [
                 "arn:aws:s3:::*"
@@ -23,6 +25,8 @@ S32CSV is a quick and dirty shell script that utilizes the AWS CLI to create a r
     ]
 }
 ```
+## Info
+Until I am able to write in the logic to accept arguments you'll need to edit the variables at the start of the script to change the behavior as you wish. Just read the comments for each variable and it should be pretty straight forward.
 ## Installation
 ```
 git clone git@github.com:rossreed/S32CSV.git
--- s32csv.sh
+++ s32csv.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
 
+### START SETTINGS VARIABLES ###
+# The 'REPORTOBJECTS' varialbe must be set to either 'true' or 'false' if set to true, the script will create a directory called 'objects' in which it will place a CSV for each bucket, containing every object in that bucket with it's URL. If a bucket is empty no report is created for it
+# WARNING: if this is set to true and you have any buckets with a large number of objects, it could take a significant amount of time for this script to run
+REPORTOBJECTS=true
 # The 'D' variable is the delimiter. If you prefer tab seperated format change the value of this to "\t" instead.
-D="\t"
-FILEPATH="report.csv"
+D=","
+# The 'FILEPATH' is where to put all the files, by default this is the current working directory, if y ou'd like the files to go somwhere else instead change the value for this, for example: FILEPATH="/path/to/put/files"
+FILEPATH=`pwd`"/s3report"
+### END SETTINGS VARIABLES ###
 
+mkdir -p $FILEPATH
+REPORTPATH=$FILEPATH"/report.csv"
 BUCKETLIST=`aws s3api list-buckets`
 
-TOTAL=`echo $BUCKETLIST | jq '.Buckets[].Name' | wc -l`
-CURRENT=0
-
-echo -e "Bucket Name"$D"Static Website Enabled"$D"Index Document"$D"Error Document"$D"Redirect To Host"$D"Date Created"$D"Region" > $FILEPATH
+if $REPORTOBJECTS ; then
+    OBJECTDIR=$FILEPATH"/objects"
+    mkdir -p $OBJECTDIR
+fi
+echo -e "Bucket Name"$D"Static Website Enabled"$D"Index Document"$D"Error Document"$D"Redirect To Host"$D"Date Created"$D"Region" > $REPORTPATH
 for BUCKET in $(echo $BUCKETLIST | jq '.Buckets[].Name' | sed 's/"//g')
 do
     echo -ne "                                                                                \r"
     echo -ne "Current Bucket: $BUCKET\r"
+    
     if WEBJSON=`aws s3api get-bucket-website --bucket $BUCKET 2> /dev/null` ; then
         REDIR=`echo $WEBJSON | jq '.RedirectAllRequestsTo.HostName' | sed 's/"//g'`
         INDEXDOC=`echo $WEBJSON | jq '.IndexDocument.Suffix' | sed 's/"//g'`
         ERRORDOC=`echo $WEBJSON | jq '.ErrorDocument.Key' | sed 's/"//g'`
-        STATICHOSTING=TRUE
+        STATICHOSTING=true
     else
-        STATICHOSTING=FALSE
+        STATICHOSTING=false
         REDIR="N/A"
         INDEXDOC="N/A"
         ERRORDOC="N/A"
     fi
+    
     if [ "$(aws s3api get-bucket-location --bucket $BUCKET | jq '.LocationConstraint')" = "null" ] ; then
         LOCATION=us-east-1
     else
-    LOCATION=`aws s3api get-bucket-location --bucket $BUCKET | jq '.LocationConstraint' | sed 's/"//g'`
+        LOCATION=`aws s3api get-bucket-location --bucket $BUCKET | jq '.LocationConstraint' | sed 's/"//g'`
     fi
     CREATEDATE=`echo $BUCKETLIST | jq -c --arg BN $BUCKET '.Buckets[] | select(.Name == $BN) | .CreationDate' | sed 's/"//g'`
-    echo -e $BUCKET$D$STATICHOSTING$D$INDEXDOC$D$ERRORDOC$D$REDIR$D$CREATEDATE$D$LOCATION >> $FILEPATH
+    echo -e $BUCKET$D$STATICHOSTING$D$INDEXDOC$D$ERRORDOC$D$REDIR$D$CREATEDATE$D$LOCATION >> $REPORTPATH
+
+    if $REPORTOBJECTS ; then
+        OBJECTLIST=`aws s3api list-objects --bucket $BUCKET --region $LOCATION | jq ".Contents[].Key"`
+        if [ -n "$OBJECTLIST" ] ; then
+            OBJECTPATH=$OBJECTDIR"/"$BUCKET".csv"
+            echo -e "Object"$D"URL" > $OBJECTPATH
+            for S3OBJECT in $(echo $OBJECTLIST | sed 's/"//g')
+            do
+                echo -e $S3OBJECT$D"HTTPS://"$BUCKET".s3.amazonaws.com/"$S3OBJECT >> $OBJECTPATH
+            done
+        fi
+    fi
 done
 echo -ne "                                                                                \r"
 echo "Done"
-echo "Report saved to $FILEPATH"
+echo "Report saved to $REPORTPATH"
